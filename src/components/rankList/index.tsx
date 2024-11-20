345a308370e99a1fc6d868a83eef16205c778754
kimminam <bluemind8520@gmail.com>
2024-11-20 23:02:21 +0900

    Feat. View for MusicChart

--- src/components/rankList/index.tsx
+++ src/components/rankList/index.tsx
@@ -1,20 +1,41 @@
-import { musicChartStore } from "../../store/store";
+import { useEffect, useState } from "react";
+import { fetchData } from "../../api";
 
-const RankList = () => {
-  const { musicChartList } = musicChartStore();
+interface siteInfo {
+  siteNames : string
+}
+
+const RankList = ({siteNames}:siteInfo) => {
+  const [musicChart, setMusicChart] = useState<object | null>(null);
+  useEffect(()=> {
+    const getData = async () => {
+    try {
+        const result = await fetchData()
+        setMusicChart(result)
+        console.log(result);
+      }
+    catch(e) {
+      console.error(e);
+    }
+  }
+    getData();
+  }, [siteNames])
+
+  if (!musicChart) return null;
+
+  if(siteNames === "") {
+    return <center>어떤 음원차트를 보여드릴까요?</center>
+  }
 
   return (
     <section>
       <ol>
-        {musicChartList.length > 0 ? (
-          musicChartList.map((chart, idx) => <li key={idx}>{chart}</li>)
-        ) : (
-          <p>No charts available</p>
-        )}
+      {Object.entries(musicChart).map(([value,key]) => (
+        <li key={value}>{JSON.stringify(key)}</li>
+        ))}
       </ol>
     </section>
   );
-  
 };
 
 export default RankList;
