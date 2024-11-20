dbdd4767bdfb1544ce4f41d25622fbd22aabe2fb
kimminam <bluemind8520@gmail.com>
2024-11-20 23:02:43 +0900

    Remove. musicChart store

--- src/store/store.ts
+++ src/store/store.ts
@@ -5,18 +5,8 @@ export interface siteInfo {
   setSelectedSite: (selectedSite: string) => void;
 }
 
-export interface musicChart {
-  musicChartList: string[];
-  setMusicChartList: (musicChartList: string[]) => void;
-}
-
 export const siteInfoStore = create<siteInfo>((set) => ({
   selectedSite: "",
   setSelectedSite: (site: string) => set({ selectedSite: site }),
-}));
-
-export const musicChartStore = create<musicChart>((set) => ({
-  musicChartList: [],
-  setMusicChartList: (musicChart: Array<string>) => set({ musicChartList: musicChart }),
 }));
 
