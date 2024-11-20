a7331470aeab1310062872f9f5ce7f68d7acef23
kimminam <bluemind8520@gmail.com>
2024-11-20 23:00:59 +0900

    Feat. context 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
-import './App.css'
-import Header from './components/header'
-import RankList from './components/rankList'
+import React, { useState } from "react";
+import "./App.css";
+import Header from "./components/header";
+import RankList from "./components/rankList";
+export const MusicSiteName = React.createContext<string | null>(null);
 
 function App() {
-
+  const [selectedSite, setSelectedSite] = useState<string>("");
   return (
-    <div id='root'>
-      <Header/>
-      <RankList/>
-    </div>
-  )
+    <MusicSiteName.Provider value={selectedSite}>
+      <div id="root">
+        <Header setSelectedSites={setSelectedSite}/>
+        <RankList siteNames={selectedSite}/>
+      </div>
+    </MusicSiteName.Provider>
+  );
 }
 
-export default App
+export default App;
